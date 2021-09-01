ad73288ed3d00912bd768663f55e701345579573
Sergey Medvedev <samedvedev@aviovert.ru>
2021-09-01 13:28:40 +0300

    works well but too slow

--- e-chip-2.cpp
+++ e-chip-2.cpp
@@ -3,26 +3,6 @@
 
 using namespace std;
 
-// функция с алгоритмом двоичного поиска 
-int Search_Binary (int arr[], int left, int right, int key)
-{
- int midd = 0;
- while (1)
- {
- midd = (left + right) / 2;
- 
- if (key < arr[midd])       // если искомое меньше значения в ячейке
- right = midd - 1;      // смещаем правую границу поиска
- else if (key > arr[midd])  // если искомое больше значения в ячейке
- left = midd + 1;    // смещаем левую границу поиска
- else                       // иначе (значения равны)
- return midd;           // функция возвращает индекс ячейки
- 
- if (left > right)          // если границы сомкнулись 
- return -1;
- }
-}
-
 int  main()
 {
     int i, j;
