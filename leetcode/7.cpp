dd977b870fb0151154c2f60e63582052ae6778e0
Sergey Medvedev <samedvedev@aviovert.ru>
2021-09-03 11:05:33 +0300

    solution for task 7

--- leetcode/7.cpp
+++ leetcode/7.cpp
@@ -1,12 +1,38 @@
 #include <iostream>
+#include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 class Solution {
 public:
-    int reverse(int x) {
-        return 0;
+    int reverse(int x) 
+    {
+        int res = 0;
+        int pop;
+
+        while( x != 0 )
+        {
+            pop = x % 10;
+            x /= 10;
+            // if( res > INT_MAX/10 || ( res == INT_MAX / 10 && pop > 7 ) ) 
+            //     return 0;
+            // if( res < INT_MIN/10 || ( res == INT_MIN / 10 && pop < -8 ) ) 
+            //     return 0;
+
+            // проверки на переполнение
+            // если результат больше max/10 или меньше min/10
+            // то при добвалении pop произойдет переполнение
+            if( res > INT_MAX/10 ) 
+                return 0;
+            if( res < INT_MIN/10 ) 
+                return 0;
+
+            res = res * 10 + pop;
+        }
+
+        return res;
     }
 };
 
@@ -33,6 +59,13 @@ int main()
         else
             cout << i+1 << " - FAIL" << endl;
     }
+
+    cout << endl;
+    cout << sol.reverse( -2147483412 ) << endl;
+    cout << endl;
+    cout << sol.reverse( -2147483648 ) << endl;
+    cout << endl;
+    cout << sol.reverse( 1534236469 ) << endl;
 
     return 0;
 }
