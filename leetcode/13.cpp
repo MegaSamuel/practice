2efb93f14bffb7e3e6ee41577457a898818f2a8e
Sergey Medvedev <samedvedev@aviovert.ru>
2021-09-03 08:43:13 +0300

    tests for task 13

--- leetcode/13.cpp
+++ leetcode/13.cpp
@@ -1,23 +1,71 @@
 #include <iostream>
-#include <string>
+#include <vector>
+
+using namespace std;
 
 class Solution {
 public:
     int romanToInt(string s) 
     {
-        return 0;
+        int tmp = 0;
+
+        for( int i = 0; i < s.size(); i++ )
+        {
+            switch( s[i] )
+            {
+                case 'M' :
+                    tmp += 1000;
+                    break;
+                case 'D' :
+                    tmp += 500;
+                    break;
+                case 'C' :
+                    tmp += 100;
+                    break;
+                case 'L' :
+                    tmp += 50;
+                    break;
+                case 'X' :
+                    tmp += 10;
+                    break;
+                case 'V' :
+                    tmp += 5;
+                    break;
+                case 'I' :
+                    tmp += 1;
+                    break;
+            }
+        }
+
+        return tmp;
     }
 };
 
 int main()
 {
-    int result;
-    string  str = "LVIII";
+    vector<int>  result;
+    vector<string>  str;
     Solution sol;
 
-    result = sol.romanToInt( str );
+    result.push_back(3);
+    result.push_back(4);
+    result.push_back(9);
+    result.push_back(58);
+    result.push_back(1994);
 
-    std::cout << result << std::endl;
+    str.push_back( "III" );
+    str.push_back( "IV" );
+    str.push_back( "IX" );
+    str.push_back( "LVIII" );
+    str.push_back( "MCMXCIV" );
+
+    for( int i = 0; i < 5; i++ )
+    {
+        if( result[i] == sol.romanToInt( str[i] ) )
+            cout << i+1 << " - OK" << endl;
+        else
+            cout << i+1 << " - FAIL" << endl;
+    }
 
     return 0;
 }
