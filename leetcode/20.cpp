e282728a5ee98dc77d82aac367726c0fe9ad7d7a
Sergey Medvedev <samedvedev@aviovert.ru>
2021-09-06 13:03:43 +0300

    task 20

--- leetcode/20.cpp
+++ leetcode/20.cpp
@@ -6,11 +6,35 @@ using namespace std;
 class Solution {
 public:
     bool isValid(string s) {
-        
+        bool result = false;
+        int open_p1 = 0, close_p1 = 0;
+        int open_p2 = 0, close_p2 = 0;
+        int open_p3 = 0, close_p3 = 0;
+
+        // solution
+
+        if( ( open_p1 == close_p1 ) && ( open_p2 == close_p2 ) && ( open_p3 == close_p3 ) )
+            result = true;
+
+        return result;
     }
 };
 
 int main()
 {
+    Solution sol;
+
+    string str1 = "()";
+    string str2 = "()[]{}";
+    string str3 = "(]";
+    string str4 = "([)]";
+    string str5 = "{[]}";
+
+    cout << sol.isValid(str1) << endl;
+    cout << sol.isValid(str2) << endl;
+    cout << sol.isValid(str3) << endl;
+    cout << sol.isValid(str4) << endl;
+    cout << sol.isValid(str5) << endl;
+
     return 0;
 }
