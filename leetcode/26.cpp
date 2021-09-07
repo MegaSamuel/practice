4c81af398666d18ee84075b7c55e57af0d5519d7
Sergey Medvedev <samedvedev@aviovert.ru>
2021-09-07 10:54:00 +0300

    task 26 : complete

--- leetcode/26.cpp
+++ leetcode/26.cpp
@@ -6,7 +6,25 @@ using namespace std;
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        return 0;
+        int i, j;
+
+        // проход по массиву
+        for( i = 0; i < nums.size(); i++ )
+        {
+            j = i;
+            // просмотр массива от текущей позиции до конца
+            // попутно удаляем элементы которые разны i-му
+            while( j+1 != nums.size() )
+            {
+               if( nums[i] == nums[j] )
+               {
+                   nums.erase( nums.begin()+j );
+               }
+               j++;
+            }
+        }
+
+        return nums.size();
     }
 };
 
@@ -15,9 +33,16 @@ int main()
     vector<int> vct;
     Solution sol;
 
+    vct.push_back(0);
+    vct.push_back(0);
+    vct.push_back(1);
     vct.push_back(1);
     vct.push_back(1);
     vct.push_back(2);
+    vct.push_back(2);
+    vct.push_back(3);
+    vct.push_back(3);
+    vct.push_back(4);
 
     cout << sol.removeDuplicates( vct ) << endl;
 
