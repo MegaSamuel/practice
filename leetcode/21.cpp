e83fdd8097f185ee1476b4de4a81a6888d176333
Sergey Medvedev <samedvedev@aviovert.ru>
2021-09-07 10:04:46 +0300

    task 21 : up

--- leetcode/21.cpp
+++ leetcode/21.cpp
@@ -24,16 +24,31 @@ public:
 
 int main()
 {
-    ListNode list_1;
-    ListNode list_2;
+    //l1 = [1,2,4], l2 = [1,3,4]
+    ListNode *list_1;
+    ListNode *list_2;
     ListNode list_res;
     Solution sol;
 
-    list_1 = ListNode();
-    list_2 = ListNode();
-    list_res = ListNode();
+    list_1 = new ListNode(1);
+    list_1->next = new ListNode(2);
+    list_1->next->next = new ListNode(4);
 
-    list_res = *(sol.mergeTwoLists( &list_1, &list_2 ));
+    list_2 = new ListNode(1);
+    list_2->next = new ListNode(3);
+    list_2->next->next = new ListNode(4);
+
+    list_res = *(sol.mergeTwoLists( list_1, list_2 ));
+
+    ListNode *tmp;
+
+    tmp = list_1;
+
+    do
+    {
+        cout << tmp->val << endl;
+        tmp = tmp->next;
+    }while( tmp->next != nullptr );
 
     return 0;
 }
