5109279f637cbefcd102d46a09ac9db5be216f6e
Sergey Medvedev <samedvedev@aviovert.ru>
2021-09-09 07:47:54 +0300

    task 21 : up

--- leetcode/21.cpp
+++ leetcode/21.cpp
@@ -13,6 +13,17 @@ struct ListNode {
 
 class Solution {
 public:
+    // печатаем значения узлов списка
+    void printList( ListNode* n )
+    {
+        while( n != nullptr )
+        {
+            cout << n->val << endl;
+            n = n->next;
+        }
+        cout << endl;
+    }
+
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
         ListNode *result;
 
@@ -27,28 +38,26 @@ int main()
     //l1 = [1,2,4], l2 = [1,3,4]
     ListNode *list_1;
     ListNode *list_2;
-    ListNode list_res;
+    ListNode *list_res;
     Solution sol;
 
+    // формируем первый список
     list_1 = new ListNode(1);
     list_1->next = new ListNode(2);
     list_1->next->next = new ListNode(4);
 
+    // формируем второй список
     list_2 = new ListNode(1);
     list_2->next = new ListNode(3);
     list_2->next->next = new ListNode(4);
 
-    list_res = *(sol.mergeTwoLists( list_1, list_2 ));
+    // сливаем списки
+    list_res = sol.mergeTwoLists( list_1, list_2 );
 
-    ListNode *tmp;
-
-    tmp = list_1;
-
-    do
-    {
-        cout << tmp->val << endl;
-        tmp = tmp->next;
-    }while( tmp->next != nullptr );
+    // вывод на экран
+    sol.printList( list_1 );
+    sol.printList( list_2 );
+    sol.printList( list_res );
 
     return 0;
 }
