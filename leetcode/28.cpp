43f943ec4092a6d7cfabbb65063e4ca8816a78d5
Sergey Medvedev <samedvedev@aviovert.ru>
2021-09-13 09:08:22 +0300

    task 28 : accepted

--- leetcode/28.cpp
+++ leetcode/28.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 #include <string>
 
 using namespace std;
@@ -6,10 +7,42 @@ using namespace std;
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        int ind = 0;
+        int  i, j;
+        bool found = false;
+        int  ind = -1;
 
-        if( haystack.empty() || needle.empty() )
+        // если подстрока пуста - выход, возврат 0
+        if( needle.empty() )
             return 0;
+        
+        // если сторока короче подстроки - выход, возврат -1
+        if( haystack.size() < needle.size() )
+            return -1;
+        
+        // проход по строке от начала до момента чтоб умещалась подстрока
+        for( i = 0; i <= haystack.size()-needle.size(); i++ )
+        {
+            // проход по подстроке
+            for( j = 0; j < needle.size(); j++ )
+            {
+                // нашли несовпадение - переходим к следующему i
+                if( haystack[i+j] != needle[j] )
+                {
+                    break;
+                }
+
+                // подстрока кончилась - успех
+                if( j == (needle.size() - 1) )
+                {
+                    found = true;
+                    ind = i;
+                }
+            }
+
+            // если нашли - выход
+            if( found )
+                break;
+        }
         
         return ind;
     }
@@ -42,6 +75,14 @@ int main()
     needle = "";
     result = sol.strStr( haystack, needle );
     if( 0 == result )
+        cout << "pass with result " << result << endl;
+    else
+        cout << "fail with result " << result << endl;
+
+    haystack = "";
+    needle = "a";
+    result = sol.strStr( haystack, needle );
+    if( -1 == result )
         cout << "pass with result " << result << endl;
     else
         cout << "fail with result " << result << endl;
