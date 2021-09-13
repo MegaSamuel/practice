3923f34d561035cdb6a95b7073a94f239d8f2e64
Sergey Medvedev <samedvedev@aviovert.ru>
2021-09-13 08:04:48 +0300

    task 35 : accepted

--- leetcode/35.cpp
+++ leetcode/35.cpp
@@ -6,34 +6,54 @@ using namespace std;
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-		unsigned left;
-		unsigned right;
-		unsigned mid;
+        bool found = false;
+		int left;
+		int right;
+        int res = 0;
+	    int mid = 0;
+
+        if( 0 == nums.size() )
+        {
+            return 0;
+        }
+
+        // if( target < nums.front() )
+        // {
+        //     return 0;
+        // }
+
+        // if( target > nums.back() )
+        // {
+        //     return nums.size();
+        // }
 
 		// начальные значения индексов
 		left = 0;
 		right = nums.size() - 1;
-		mid = ( left + right ) / 2;
 
 		while( left <= right )
 		{
 			mid = ( left + right ) / 2; // индекс середины
 
-    		// if( nums[mid] == target ) // элемент с индексом mid равен ключу
-			//   break;
-    		
-			// if( nums[mid] > target ) 
-			// 	right = mid - 1;  // элемент с индексом mid больше ключа - переносим правую границу
-    		// else 
-			// 	left = mid + 1;   // элемент с индексом mid меньше ключа - переносим левую границу
+            if( nums[mid] == target )
+            {
+                res = mid;
+                break;
+            }
 
-			if( nums[mid] >= target ) 
-				break;
+			if( nums[mid] > target )
+            {
+                right = mid - 1;   // элемент с индексом mid больше цели - переносим правую границу
+            }
     		else 
-				left = mid + 1;   // элемент с индексом mid меньше ключа - переносим левую границу
+            {
+				left = mid + 1;   // элемент с индексом mid меньше цели - переносим левую границу
+            }
+
+            res = left;
 		}
 
-		return mid;
+		return res;
     }
 };
 
@@ -78,6 +98,58 @@ int main()
 
 	result = sol.searchInsert( nums, target );
 	if( 4 == result )
+		cout << "pass with result " << result << endl;
+	else
+		cout << "fail with result " << result << endl;
+
+	nums.clear();
+	nums.push_back(1);
+	nums.push_back(3);
+	nums.push_back(5);
+	nums.push_back(6);
+	target = 0;
+
+	result = sol.searchInsert( nums, target );
+	if( 0 == result )
+		cout << "pass with result " << result << endl;
+	else
+		cout << "fail with result " << result << endl;
+
+	nums.clear();
+	nums.push_back(1);
+	nums.push_back(3);
+	nums.push_back(5);
+	target = 1;
+
+	result = sol.searchInsert( nums, target );
+	if( 0 == result )
+		cout << "pass with result " << result << endl;
+	else
+		cout << "fail with result " << result << endl;
+
+	nums.clear();
+	nums.push_back(1);
+    nums.push_back(2);
+	nums.push_back(3);
+    nums.push_back(4);
+	nums.push_back(5);
+    nums.push_back(10);
+	target = 2;
+
+	result = sol.searchInsert( nums, target );
+	if( 1 == result )
+		cout << "pass with result " << result << endl;
+	else
+		cout << "fail with result " << result << endl;
+
+	nums.clear();
+	nums.push_back(1);
+	nums.push_back(3);
+	nums.push_back(5);
+	target = 3;
+
+	result = sol.searchInsert( nums, target );
+	if( 1 == result )
 		cout << "pass with result " << result << endl;
 	else
 		cout << "fail with result " << result << endl;
