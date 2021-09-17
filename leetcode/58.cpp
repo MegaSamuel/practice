775a98cc2f29105f022150df0cee502881b46573
Sergey Medvedev <samedvedev@aviovert.ru>
2021-09-17 08:13:19 +0300

    task 58 : tests

--- leetcode/58.cpp
+++ leetcode/58.cpp
@@ -1,14 +1,37 @@
 #include <iostream>
 #include <string>
+#include <vector>
+
+using namespace std;
 
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        
+        return 0;
     }
 };
 
 int main()
 {
+    Solution sol;
+    vector<string> vctStr;
+    vector<int> vctLength;
+
+    vctStr.push_back("Hello World");
+    vctStr.push_back("   fly me   to   the moon  ");
+    vctStr.push_back("luffy is still joyboy");
+
+    vctLength.push_back(5);
+    vctLength.push_back(4);
+    vctLength.push_back(6);
+
+    for( int i = 0; i < vctStr.size(); i++ )
+    {
+        int result = sol.lengthOfLastWord(vctStr[i]);
+        if( vctLength[i] == result )
+            cout << i+1 << " - PASS" << endl;
+        else
+            cout << i+1 << " - FAIL with res " << result << endl;
+    }
     
 }
