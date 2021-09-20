b1f2be31e38163b405003ccd9ca07f8d8d0457f4
Sergey Medvedev <samedvedev@aviovert.ru>
2021-09-20 11:45:33 +0300

    task 66 : add tests

--- leetcode/66.cpp
+++ leetcode/66.cpp
@@ -6,11 +6,53 @@ using namespace std;
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        
+        vector<int> vct;
+
+        for( const int& it : vct )
+        {
+            cout << it << " ";
+        }
+        cout << endl;
+
+        return vct;
     }
 };
 
 int main()
 {
     Solution sol;
+    vector<int> vct_in;
+    vector<int> vct_out;
+
+    vector<pair<vector<int>, vector<int>>> data;
+
+    vct_in = {1,2,3};
+    vct_out = {1,2,4};
+    data.push_back( pair(vct_in, vct_out) );
+
+    vct_in = {4,3,2,1};
+    vct_out = {4,3,2,2};
+    data.push_back( pair(vct_in, vct_out) );
+
+    vct_in = {0};
+    vct_out = {1};
+    data.push_back( pair(vct_in, vct_out) );
+
+    vct_in = {9};
+    vct_out = {1,0};
+    data.push_back( pair(vct_in, vct_out) );
+
+    for( int i = 0; i < data.size(); i++ )
+    {
+        if( data[i].second == sol.plusOne( data[i].first ) )
+        {
+            cout << i << " - PASS" << endl;
+        }
+        else
+        {
+            cout << i << " - FAIL" << endl;
+        }
+    }
+
+    return 0;
 }
