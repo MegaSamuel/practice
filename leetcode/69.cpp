e80485cf30a85ed064db486409e89d03810da54a
Sergey Medvedev <samedvedev@aviovert.ru>
2021-09-21 11:20:25 +0300

    task 69 : accepted

--- leetcode/69.cpp
+++ leetcode/69.cpp
@@ -6,15 +6,32 @@ using namespace std;
 class Solution {
 public:
     int mySqrt(int x) {
-        return x;
+        long long i = 0;
+
+        while( i <= x )
+        {
+            i++;
+
+            if( i*i > x )
+            {
+                return i-1;
+            }
+
+            if( i*i == x )
+            {
+                return i;
+            }
+        }
+
+        return i;
     }
 };
 
 int main()
 {
     Solution sol;
-    vector<int> vct_in = {4, 8};
-    vector<int> vct_out = {2, 2};
+    vector<int> vct_in = {0, 1, 4, 8, 6, 2147483647};
+    vector<int> vct_out = {0, 1, 2, 2, 2, 46340 };
 
     for( int i = 0; i < vct_in.size(); i++ )
     {
