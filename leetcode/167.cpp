0a20eb3d4177551bdb821ce8c2ab752db823e8f3
Sergei Medvedev <cepaleme@gmail.com>
2021-09-24 17:42:02 +0300

    task 167 : add test

--- leetcode/167.cpp
+++ leetcode/167.cpp
@@ -6,11 +6,28 @@ using namespace std;
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        
+        vector<int>  out;
+
+        // for( int i = 0; i < numbers.size()/2; i++ )
+        // {
+        //     if( target < numbers[i] + numbers[numbers.size()-1-i] )
+        //     {
+
+        //     }
+        // }
+
+        return out;        
     }
 };
 
 int main()
 {
-    
+    Solution sol;
+    int target = 9;
+    vector<int> numbers = {2,7,11,15};
+
+    for( auto it : sol.twoSum( numbers, target ) )
+    {
+        cout << it << " ";
+    }
 }
