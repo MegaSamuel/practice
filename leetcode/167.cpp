c497058d1be4785954b61c2664059755b406243a
Sergey Medvedev <samedvedev@aviovert.ru>
2021-09-24 15:35:31 +0300

    task 167 : draft

---
+++ leetcode/167.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        
+    }
+};
+
+int main()
+{
+    
+}
