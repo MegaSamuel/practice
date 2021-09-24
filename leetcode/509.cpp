b00291b12b96b0fe7a356ca5f71af31bc68d5ec6
Sergey Medvedev <samedvedev@aviovert.ru>
2021-09-24 12:07:13 +0300

    task 509 : draft

---
+++ leetcode/509.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int fib(int n) {
+        return 0;
+    }
+};
+
+int main()
+{
+    vector<int>  input  = {0,1,2,3,4,5};
+    vector<int>  output = {0,1,1,2,3,5};
+    Solution sol;
+
+    for( int i = 0; i < static_cast<int>(input.size()); i++ )
+    {
+        if( output[i] == sol.fib( input[i] ) )
+            cout << i+1 << " - OK" << endl;
+        else
+            cout << i+1 << " - FAIL" << endl;
+    }
+}
