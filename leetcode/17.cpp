c070f5c14aa888b85c7c1ede4716e9397ad1d339
Sergei Medvedev <cepaleme@gmail.com>
2021-10-06 22:25:58 +0300

    task 17 : draft

---
+++ leetcode/17.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        
+    }
+};
+
+int main()
+{
+    Solution sol;
+
+    cout << sol.letterCombinations("") << endl;
+
+}
