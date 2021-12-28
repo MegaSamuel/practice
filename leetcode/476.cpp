c3330631b51e32da6b62b16661ae2484cb5cb119
Sergey Medvedev <samedvedev@aviovert.ru>
2021-12-28 16:01:28 +0300

    task 476 : draft

---
+++ leetcode/476.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int findComplement(int num) {
+        return 0;
+    }
+};
+
+int main()
+{
+    Solution sol;
+
+    cout << sol.findComplement(5);
+}
