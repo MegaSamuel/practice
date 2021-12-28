2d893e887d4ee07582604748767e9d1fd862db05
Sergey Medvedev <samedvedev@aviovert.ru>
2021-12-28 10:28:08 +0300

    task 83 : solution

--- leetcode/83.cpp
+++ leetcode/83.cpp
@@ -15,19 +15,49 @@ struct ListNode {
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-        return head;        
+        ListNode *current = head;
+
+        while(nullptr != current)
+        {
+            // while next node exists
+            while(nullptr != current->next)
+            {
+                // if current value and next value is the same
+                if(current->val == current->next->val)
+                {
+                    current->next = current->next->next;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            // going to the next node
+            current = current->next;
+        }
+
+        // return the head of a new list
+        return head;
     }
 };
 
 int main()
 {
-//    [1,1,2,3,3]
-
+#if 1
+    //[1,1,2,3,3]
     ListNode node5(3, nullptr);
     ListNode node4(3, &node5);
     ListNode node3(2, &node4);
     ListNode node2(1, &node3);
     ListNode node1(1, &node2);
+#else
+    //[1,1,1,1]
+    ListNode node4(1, nullptr);
+    ListNode node3(1, &node4);
+    ListNode node2(1, &node3);
+    ListNode node1(1, &node2);
+#endif
 
     Solution sol;
 
