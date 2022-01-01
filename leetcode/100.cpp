3c41bf3b816a92bf426d497aafa8eb3db5973a86
Sergei Medvedev <cepaleme@gmail.com>
2022-01-01 14:51:26 +0300

    task 100 : accepted

--- leetcode/100.cpp
+++ leetcode/100.cpp
@@ -17,6 +17,67 @@ struct TreeNode {
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
+        int depth1;
+        int depth2;
+        TreeNode *current1 = p;
+        TreeNode *current2 = q;
+        stack<TreeNode*> stackNode1;
+        stack<TreeNode*> stackNode2;
+
+        if((nullptr == p) && (nullptr == q))
+            return true;
+
+        if(nullptr == p)
+            return false;
+
+        if(nullptr == q)
+            return false;
+
+        while((nullptr != current1) || (nullptr != current2) || !stackNode1.empty() || !stackNode2.empty())
+        {
+            depth1 = 0;
+            while(nullptr != current1)
+            {
+                depth1++;
+
+                stackNode1.push(current1);
+
+                current1 = current1->left;
+            }
+
+            depth2 = 0;
+            while(nullptr != current2)
+            {
+                depth2++;
+
+                stackNode2.push(current2);
+
+                current2 = current2->left;
+            }
+
+            if(depth1 != depth2)
+            {
+                return false;
+            }
+
+            current1 = stackNode1.top();
+            current2 = stackNode2.top();
+
+            if(current1->val != current2->val)
+                return false;
+
+            stackNode1.pop();
+            stackNode2.pop();
+
+            current1 = current1->right;
+            current2 = current2->right;
+        }
+
+        if(!stackNode1.empty() || !stackNode2.empty())
+        {
+            return false;
+        }
+
         return true;
     }
 };
